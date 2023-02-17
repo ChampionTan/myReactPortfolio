29ee2bd1d42946bdb01e8a5f268f7448cc3aaa20
unknown <tannerwlumsden@gmail.com>
2023-02-17 11:44:57 -0500

    started over

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,18 +1,8 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
+import React from 'react';
+import Homepage from "./components/Homepage";
 
-export default function App() {
-	return (
-		<main>
-			<Navbar />
-			<About />
-			<Projects />
-			<Skills />
-			<Contact />
-		</main>
-	);
-}
+function App() {
+  return ()
+    
+};
+export default App;
