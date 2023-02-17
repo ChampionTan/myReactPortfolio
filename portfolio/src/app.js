242514ac62df3bccc7c634f04043b2693f711e9d
unknown <tannerwlumsden@gmail.com>
2023-02-17 12:21:51 -0500

    created header page

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Homepage from "./components/Homepage";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+
+
 
 function App() {
-  return ()
+  return (
+    <main>
+      <Homepage />
+      <Navbar />
+      <Projects />
+      <Contact />
+    </main>
+  )
     
 };
 export default App;
---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from '../Navigation';
+import "./index.css";
+
+export default function Header ({handlePageChange, pages, currentPage}) {
+	return(
+		<>
+		<header className="header">
+			<h1 className="name text-center">Tanner Lumsden</h1>
+			<Navigation
+			currentPage={currentPage}
+			handlePageChange={handlePageChange}
+			pages={pages}
+			/>
+			</header>
+			</>
+	);
+}
